5188fd6619c81cd494134c0d3584567ef32141d9
Impactivity <minwoo2944@gmail.com>
2022-06-01 12:09:12 +0900

    6.1

--- 9037.py
+++ 9037.py
@@ -2,7 +2,7 @@ import sys
 
 read = sys.stdin.readline
 T = int(read())
-answer = []
+
 def add_candy():
     length = len(C)
     for i in range(length):
@@ -11,7 +11,7 @@ def add_candy():
 
 def ditribute():
     length = len(C)
-    after = [0] * length
+    after = [0] * length # 분배한 갯수 저장하는 리스트
     for i in range(length):
         after[(i+1) % length] = C[i] // 2
         C[i] = C[i] // 2
@@ -19,37 +19,20 @@ def ditribute():
     for i in range(length):
         C[i] += after[i]
 
-def check():
-    length = len(C)
-    for i in range(1,length):
-        if C[i] != C[i-1]:
-            return False
-    return True
+def check(): #모두다 같은 갯수인지 체크
+    return len(set(C)) == 1
+
 
 for _ in range(T):
-    #아이들의 수
-    N = int(read())
-    # 아이들이 초기에 가지고있는 사탕의 갯수
-    C = list(map(int,read().split()))
+    # N :아이들의 수 , C : 아이들이 초기에 가지고있는 사탕의 갯수
+    N,C = int(read()),list(map(int,read().split()))
     circle = 0
     # 초기 홀수는 짝수로 만들어준다.
     add_candy()
-    is_True = check()
-    if is_True == True:
-        answer.append(circle)
-        continue
-
-    while True:
+    while not check():
         ditribute() #분배
         add_candy() #홀수->짝수
         circle+=1 # 순환 + 1
-        is_True = check()
-        if is_True:
-            break
-
-    answer.append(circle)
-
-for num in answer:
-    print(num)
+    print(circle)
 
 
